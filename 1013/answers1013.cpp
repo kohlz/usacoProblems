13124a861dcbc4da28b25583d908dccf9d2f2f6a
kohlz <2115385375@qq.com>
2021-10-25 18:42:15 +0800

    1013

--- 1013/answers1013.cpp
+++ 1013/answers1013.cpp
@@ -5,18 +5,14 @@ using namespace std;
 int change[2][2];
 
 void exchange(int a){
-    cout<<a<<endl;
     if(a >= change[0][0] && a <= change[0][1]){
         // x - left = right - y
         // y = right - x + left
         a = change[0][1] - a + change[0][0];
-        cout<<1<<endl;
     }
     if(a >= change[1][0] && a <= change[1][1]){
         a = change[1][1] - a + change[1][0];
-        cout<<2<<endl;
     }
-    cout<<endl;
     return;
 }
 
@@ -30,22 +26,16 @@ int main(){
     int temp = 0;
     for(int i=0; i<2; i++){
         cin>>change[i][0]>>change[i][1];
-//      cout<<change[i][0]<<" "<<change[i][1]<<endl;
     }
-    // cout<<endl;
 
     for(int i=1; i<=k; i++){
         temp = i;
-        for(int j=0; j<2; j++){
+        for(int j=0; j<n; j++){
             exchange(temp);
-            // cout<<temp<<endl;
         }
-        // cout<<endl;
-        // cout<<temp<<endl;
         fin[temp] = i;
     }
 
-    // cout<<endl;
     for(int i=1; i<=k; i++){
         cout<<fin[i]<<endl;
     }
@@ -53,3 +43,4 @@ int main(){
     return 0;
 }
 // code not complete yet
+// problem will be solved predictly in 2021.10.29
